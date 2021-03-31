43ac6c5c1ac550a800b8afb76ea31dc734c5fc06
reoge <reoge@mail.ru>
2021-03-31 23:56:21 +0300

    Add two custom type hints to denote positions of visual stimuli on screen

--- base/task_views.py
+++ base/task_views.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from collections import namedtuple
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from pathlib import Path
 
 import numpy as np
@@ -24,6 +24,10 @@ def _ensure_creation_of_element(obj_creation_function: visual.basevisual):
                 pass
 
     return wrapper
+
+
+RealNumber = Union[int, float]
+ScreenPosition = Tuple[RealNumber, RealNumber]
 
 
 class AbstractTaskView(metaclass=ABCMeta):
